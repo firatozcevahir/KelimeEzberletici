e870831352843afece25cc08529f13e19604272c
Fırat Özcevahir <firat.ozcevahir@gmail.com>
2019-03-20 20:16:28 +0300

    Proje Baslangic

---
+++ Ezberletici/KelimeIslemleri.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Ezberletici
+{
+    partial class KelimeIslemleri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // KelimeIslemleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 309);
+            this.Controls.Add(this.textBox1);
+            this.Name = "KelimeIslemleri";
+            this.Text = "Kelime İşlemleri";
+            this.Load += new System.EventHandler(this.KelimeIslemleri_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Ezberletici/KelimeIslemleri.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ezberletici
+{
+    public partial class KelimeIslemleri : Form
+    {
+        public KelimeIslemleri()
+        {
+            InitializeComponent();
+        }
+
+        private void KelimeIslemleri_Load(object sender, EventArgs e)
+        {
+            string connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=KelimeEzberleticiDb.accdb;Persist Security Info=False;";
+            OleDbConnection conn = new OleDbConnection(connString);
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = conn;
+            command.CommandText = "SELECT * FROM Words";
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    MessageBox.Show(reader["Lang_TR"].ToString());
+                }
+            }
+            
+            conn.Close();
+        }
+    }
+}
